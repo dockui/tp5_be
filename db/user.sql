0f44914c4eecb1da6279718c18fc552d57bbbac5
caobo <caobo@caobodeMac-mini.local>
2018-08-29 19:34:01 +0800

    nickname

---
+++ db/user.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS `T_USER`;
+CREATE TABLE `T_USER` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nickname` varchar(100) DEFAULT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `salt` char(3) DEFAULT NULL,
+  `mobile` varchar(11) DEFAULT NULL,
+  `status` tinyint(4) DEFAULT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
+  `openid` varchar(64) DEFAULT NULL comment '微信openid',
+  `last_login_ip` varchar(100) DEFAULT NULL,
+  `last_login_time` int(11) DEFAULT NULL,
+  `expire_time` int(11) DEFAULT NULL,
+  `create_time` int(11) DEFAULT NULL,
+  `update_time` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
