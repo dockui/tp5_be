5b109fa400faecb064b5d4bc99cece3b02aa6337
caobo <caobo@caobodeMac-mini.local>
2018-08-30 17:38:46 +0800

    user tb

--- application/index/model/User.php
+++ application/index/model/User.php
@@ -10,4 +10,63 @@ class User extends Model
     protected $table = 'T_USER';
 // protected $connection = 'database_user';
 
+   //默认时间格式
+    protected $dateFormat = 'Y-m-d H:i:s';
+
+    protected $type       = [
+        // 设置时间戳类型（整型）
+        'create_time'     => 'timestamp',
+        'update_time'     => 'timestamp',
+        'last_login_time' => 'timestamp',
+        'expire_time' => 'timestamp',
+
+    ];
+
+    //自动完成
+    protected $insert = [
+    	'create_time',
+    	'update_time',
+    ];
+
+    protected $update = ['update_time'];
+
+    // 属性修改器
+    protected function setCreateTimeAttr($value, $data)
+    {
+        return time();
+    }
+
+    // 属性修改器
+    protected function setUpdateTimeAttr($value, $data)
+    {
+        return time();
+    }
+
+    // 属性修改器
+    protected function setLastLoginTimeAttr($value, $data)
+    {
+        return time();
+    }
+
+    protected function getLastLoginTimeAttr($datetime)
+    {
+        return date('Y-m-d H:i:s', $datetime);
+    }
+
+    protected function setExpireTimeAttr($value, $data)
+    {
+        return time();
+    }
+    protected function getExpireTimeAttr($datetime)
+    {
+        return date('Y-m-d H:i:s', $datetime);
+    }
+
+    // status属性读取器
+    protected function getStatusAttr($value)
+    {
+        $status = [-1 => '删除', 0 => '禁用', 1 => '正常', 2 => '待审核'];
+        return $status[$value];
+    }
+    
 }
